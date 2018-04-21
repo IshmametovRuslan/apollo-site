c8c57a27fc3589e039c7b16884012b5e070e8350
Ruslan Ishmametov <russ-i@yandex.ru>
2018-04-21 16:11:02 +0300

    1. Layout of slides 2. Adding jquery 3. Menu layout

--- index.php
+++ index.php
@@ -5,16 +5,30 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/scripts.js"></script>
+	<link rel="stylesheet" href="css/style.css">
 	<title>Apollo</title>
 </head>
 <body>
 <div class="main-container">
-	<div class="home"></div>
-	<div class="about"></div>
-	<div class="portfolio-showcase-1"></div>
-	<div class="portfolio-showcase-2"></div>
-	<div class="portfolio-showcase-3"></div>
-	<div class="contact"></div>
+	<div class="home slide">
+		<div class="menu">
+			<div id="burger-container">
+				<div id="burger">
+					<span> &nbsp;</span>
+					<span> &nbsp;</span>
+					<span> &nbsp;</span>
+					<span> &nbsp;</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="about slide"></div>
+	<div class="portfolio-showcase-1 slide"></div>
+	<div class="portfolio-showcase-2 slide"></div>
+	<div class="portfolio-showcase-3 slide"></div>
+	<div class="contact slide"></div>
 </div>
 
 </body>
