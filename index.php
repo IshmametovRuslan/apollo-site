b93405696054ceb300b519ffd5d89b719bb5056f
Ruslan <russ-i@yandex.ru>
2018-05-11 01:28:22 +0300

    Основня верстка завершена, необходимы доработки, исправления ошибок и "натяжка" на CMS

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/style-slider.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/swiper.min.js"></script>
 	<script src="js/jquery-3.3.1.min.js"></script>
 	<script src="js/scripts.js"></script>
-	<link rel="stylesheet" href="css/style.css">
 	<title>Apollo</title>
 </head>
 <body>
@@ -77,24 +81,164 @@
 	</div>
 </div>
 <div class="portfolio-showcase-1 slide">
-	<div class="prev n"><img src="/images/slider/slider-icon/back (1).png" alt=""></div>
+	<div class="swiper-button-prev swiper-button-white prev" id="n"></div>
 	<div class="slider">
 		<div class="img curry"></div>
 		<div class="img"></div>
 		<div class="img"></div>
 		<div class="img"></div>
 	</div>
-	<div class="next n"><img src="/images/slider/slider-icon/next (1).png" alt=""></div>
+	<div class="swiper-button-next swiper-button-white next" id="n"></div>
 	<div class="left-content"></div>
 </div>
 <div class="portfolio-showcase-2 slide">
 	<div class="portfolio-showcase-2-left">
-		
+		<div class="portfolio-showcase-2-text">
+			<p class="head"><span>Ut enim</span> ad minim</p>
+			<p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Aliquam aspernatur dolor eaque, enim excepturi sit veniam.
+				Ab dolorum explicabo laboriosam, laudantium
+				non porro rem reprehenderit repudiandae saepe sed. Dolores, ipsam?</p>
+		</div>
+
 	</div>
-	<div class="portfolio-showcase-2-right"></div>
+	<div class="portfolio-showcase-2-right">
+		<div class="portfolio-showcase-2-right-img">
+
+		</div>
+	</div>
 </div>
-<div class="portfolio-showcase-3 slide"></div>
-<div class="contact slide"></div>
+<div class="portfolio-showcase-3 slide">
+	<div class="left-content">
+
+	</div>
+	<div class="swiper-container main-slider loading">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://www.villreal.com/img47/fgicyptcdbjnpfgmdyzz.jpg)">
+					<img src="http://www.villreal.com/img47/fgicyptcdbjnpfgmdyzz.jpg" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Shaun Matthews</p>
+					<span class="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://www.fonstola.ru/pic/201112/1920x1200/fonstola.ru-64722.jpg)">
+					<img src="http://www.fonstola.ru/pic/201112/1920x1200/fonstola.ru-64722.jpg" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Alexis Berry</p>
+					<span class="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://8-themes.com/wp-content/uploads/2015/11/Animals___Wild_cats_Black_and_white_photo_of_a_lion_095183_-1920x1080.jpg)">
+					<img src="http://8-themes.com/wp-content/uploads/2015/11/Animals___Wild_cats_Black_and_white_photo_of_a_lion_095183_-1920x1080.jpg"
+					     class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Billie Pierce</p>
+					<span class="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://macdonaldmp.com/wp-content/uploads/2014/09/buildings.jpg)">
+					<img src="http://macdonaldmp.com/wp-content/uploads/2014/09/buildings.jpg" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Trevor Copeland</p>
+					<span class="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg" style="background-image:url(https://pxhst.co/avaxhome/78/5e/003d5e78.png)">
+					<img src="https://pxhst.co/avaxhome/78/5e/003d5e78.png" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Bernadette Newman</p>
+					<span class="caption">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+				</div>
+			</div>
+		</div>
+		<!-- If we need navigation buttons -->
+		<div class="swiper-button-prev swiper-button-white"></div>
+		<div class="swiper-button-next swiper-button-white"></div>
+	</div>
+
+	<!-- Thumbnail navigation -->
+	<div class="swiper-container nav-slider loading">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://www.villreal.com/img47/fgicyptcdbjnpfgmdyzz.jpg)">
+					<img src="http://www.villreal.com/img47/fgicyptcdbjnpfgmdyzz.jpg" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Shaun Matthews</p>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://www.fonstola.ru/pic/201112/1920x1200/fonstola.ru-64722.jpg)">
+					<img src="http://www.fonstola.ru/pic/201112/1920x1200/fonstola.ru-64722.jpg" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Alexis Berry</p>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://8-themes.com/wp-content/uploads/2015/11/Animals___Wild_cats_Black_and_white_photo_of_a_lion_095183_-1920x1080.jpg)">
+					<img src="http://8-themes.com/wp-content/uploads/2015/11/Animals___Wild_cats_Black_and_white_photo_of_a_lion_095183_-1920x1080.jpg"
+					     class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Billie Pierce</p>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg"
+				        style="background-image:url(http://macdonaldmp.com/wp-content/uploads/2014/09/buildings.jpg)">
+					<img src="http://macdonaldmp.com/wp-content/uploads/2014/09/buildings.jpg" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Trevor Copeland</p>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<figure class="slide-bgimg" style="background-image:url(https://pxhst.co/avaxhome/78/5e/003d5e78.png)">
+					<img src="https://pxhst.co/avaxhome/78/5e/003d5e78.png" class="entity-img"/>
+				</figure>
+				<div class="content">
+					<p class="title">Bernadette Newman</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="contact slide">
+	<div class="contact-text">
+		<div class="head"><span>Say</span> Hello</div>
+		<div class="text">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci animi blanditiis
+				consequatur culpa cumque, dolore facilis hic illo labore necessitatibus, nesciunt nobis numquam quasi
+				quia repellat vitae voluptates voluptatibus.</p>
+		</div>
+	</div>
+	<div class="contact-form">
+		<form action="">
+			<input type="email" class="email" placeholder="Введите Ваш email">
+			<input type="text" name="name" class="name" placeholder="Введите Ваше имя">
+			<input type="text" name="message"  class="message" placeholder="Введите текст сообщения">
+			<input type="button" class="send" value="Отправить">
+		</form>
+	</div>
+</div>
 
 
 </body>
